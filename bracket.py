200332f8750fd9d0ff645d51bf92191fb2583fcc
Eric Larson <elarson@akamai.com>
2022-04-16 13:08:33 -0400

    Add bracket.py

---
+++ bracket.py
@@ -0,0 +1,108 @@
+
+""" 2022 Men's NCAA Tournament Bracket """ 
+bracket = [
+    
+    # West Region
+    ('GONZ', 1),
+    ('GAST', 16),
+    
+    ('BSU', 8),
+    ('MEM', 9),
+    
+    ('CONN', 5),
+    ('NMSU', 12),    
+    
+    ('ARK', 4),
+    ('UVM', 13),
+    
+    ('ALA', 6),
+    ('ND', 11),        
+
+    ('TTU', 3),
+    ('MTST', 14),        
+    
+    ('MSU', 7),
+    ('DAV', 10),        
+    
+    ('DUKE', 2),
+    ('CSUF', 15),    
+    
+ 
+    # East Region
+    ('BAY', 1),
+    ('NORF', 16),
+    
+    ('UNC', 8),
+    ('MARQ', 9),
+    
+    ('SMC', 5),
+    ('IU', 12),    
+    
+    ('UCLA', 4),
+    ('ARK', 13),
+    
+    ('TEX', 6),
+    ('VT', 11),        
+
+    ('PUR', 3),
+    ('YALE', 14),        
+    
+    ('MUR', 7),
+    ('SF', 10),        
+    
+    ('UK', 2),
+    ('SPU', 15),        
+
+
+    # South Region
+    ('ARIZ', 1),
+    ('WSU', 16),
+    
+    ('HALL', 8),
+    ('TCU', 9),
+    
+    ('HOU', 5),
+    ('UAB', 12),    
+    
+    ('ILL', 4),
+    ('UTC', 13),
+    
+    ('CSU', 6),
+    ('MICH', 11),        
+
+    ('TENN', 3),
+    ('LONG', 14),        
+    
+    ('OSU', 7),
+    ('LUC', 10),        
+    
+    ('VILL', 2),
+    ('DEL', 15), 
+    
+    # Midwest Region
+    ('KU', 1),
+    ('TXS', 16),
+    
+    ('SDSU', 8),
+    ('CREI', 9),
+    
+    ('IOWA', 5),
+    ('RICH', 12),    
+    
+    ('PROV', 4),
+    ('SDST', 13),
+    
+    ('LSU', 6),
+    ('ISU', 11),        
+
+    ('WISC', 3),
+    ('COLG', 14),        
+    
+    ('USC', 7),
+    ('MIA', 10),        
+    
+    ('AUB', 2),
+    ('JVST', 15),     
+
+]
+
